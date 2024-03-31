f41cdb381de9d0f3581209d348541c236d1cc653
Tavanduc411998 <tavanduc2018@gmail.com>
2024-04-01 06:24:33 +0700

    no js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import Header from "./assets/js/header.js";
 import Body from "./assets/js/body.js";
 import Footer from "./assets/js/footer.js";
 
-document.querySelector("#header-container").innerHTML = Header();
-document.querySelector("#body-container").innerHTML = Body();
-document.querySelector("#footer-container").innerHTML = Footer();
+// document.querySelector("#header-container").innerHTML = Header();
+// document.querySelector("#body-container").innerHTML = Body();
+// document.querySelector("#footer-container").innerHTML = Footer();
 
 
